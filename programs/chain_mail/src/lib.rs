c7797bcd7767ebe926c8afd2fc5417e0746fe786
myware <martin.a.molina@adaptivealphacapital.com>
2021-11-19 19:54:01 +0800

    storing boolean state to data_account

--- programs/chain_mail/src/lib.rs
+++ programs/chain_mail/src/lib.rs
@@ -2,13 +2,78 @@ use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+fn print_type_of<T>(_: &T) {
+    msg!("{}", std::any::type_name::<T>())
+}
+
 #[program]
 pub mod chain_mail {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        let state_account = &mut ctx.accounts.state_account;
+        state_account.candidate_registration_is_active = false;
+        Ok(())
+    }
+    pub fn open_candidate_registration(ctx: Context<OpenCandidateRegistration>) -> ProgramResult {
+        let state_account = &mut ctx.accounts.state_account;
+        if state_account.candidate_registration_is_active == true {
+            msg!("candidate registration is already open");
+        }
+        else {
+            state_account.candidate_registration_is_active = true;
+            msg!("opened candidate registration");
+        }
+        Ok(())
+    }
+    pub fn register_candidate(ctx: Context<RegisterCandidate>) -> ProgramResult {
+        print_type_of(&ctx);
+        Ok(())
+    }
+    pub fn unregister_candidate(ctx: Context<UnregisterCandidate>) -> ProgramResult {
+        print_type_of(&ctx);
+        Ok(())
+    }
+    pub fn close_candidate_registration(ctx: Context<CloseCandidateRegistration>) -> ProgramResult {
+        let state_account = &mut ctx.accounts.state_account;
+        if state_account.candidate_registration_is_active == true {
+            state_account.candidate_registration_is_active = false;
+            msg!("closed candidate account registration");
+        }
+        else {
+            msg!("closed candidate account registration already closed");
+        }
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 1)]
+    pub state_account: Account<'info, StateAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct OpenCandidateRegistration<'info> {
+    #[account(mut)]
+    pub state_account: Account<'info, StateAccount>
+}
+
+#[derive(Accounts)]
+pub struct RegisterCandidate {}
+
+#[derive(Accounts)]
+pub struct UnregisterCandidate {}
+
+#[derive(Accounts)]
+pub struct CloseCandidateRegistration<'info> {
+    #[account(mut)]
+    pub state_account: Account<'info, StateAccount>
+}
+
+#[account]
+pub struct StateAccount {
+    pub candidate_registration_is_active: bool,
+}
--- tests/chain_mail.ts
+++ tests/chain_mail.ts
@@ -5,13 +5,45 @@ import { ChainMail } from '../target/types/chain_mail';
 describe('chain_mail', () => {
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  // anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.local();
+
+  // setup state account
+  const stateAccount = anchor.web3.Keypair.generate();
 
   const program = anchor.workspace.ChainMail as Program<ChainMail>;
 
   it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+
+    const tx0 = await program.rpc.initialize({
+      accounts: {
+        stateAccount: stateAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [stateAccount],
+    });
+    console.log("Your transaction 0 signature", tx0);
+
+    // const tx1 = await program.rpc.openCandidateRegistration({
+    //   accounts: {
+    //     stateAccount: stateAccount.publicKey,
+    //   }
+    // });
+    // console.log("Your transaction 1 signature", tx1);
+    //
+    // const tx2 = await program.rpc.registerCandidate({});
+    // console.log("Your transaction 2 signature", tx2);
+    //
+    // const tx3 = await program.rpc.unregisterCandidate({});
+    // console.log("Your transaction 3 signature", tx3);
+    //
+    // const tx4 = await program.rpc.closeCandidateRegistration({
+    //   accounts: {
+    //     stateAccount: stateAccount.publicKey,
+    //   }
+    // });
+    // console.log("Your transaction 4 signature", tx4);
+
   });
 });
